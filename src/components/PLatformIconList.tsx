1f277cba24795a300dc3a29a603568706b2d84be
Sara Fotros <55629228+sarafotros@users.noreply.github.com>
2023-03-25 22:20:15 +0000

    displaying critic score

---
+++ src/components/PLatformIconList.tsx
@@ -0,0 +1,40 @@
+import { Text, Icon, HStack } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+const PLatformIconList = ({ platforms }: Props) => {
+    const iconMap: { [key: string]: IconType } = { 
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        linux: FaLinux, 
+        android: FaAndroid,
+        ios: MdPhoneIphone,
+        web: BsGlobe
+      }
+  return (
+    <HStack marginY={'10px'}>
+      {platforms.map((platform) => (
+       <Icon as={iconMap[platform.slug]} color='gray.500'/>
+      ))}
+    </HStack>
+  );
+};
+
+export default PLatformIconList;
